f6336f8da19af99e8f3ea746e0eb637e22ec4a10
jishnugupta25-prog <jishnugupta25@gmail.com>
2025-09-11 03:46:14 +0530

    Create createAdmin.ts

---
+++ createAdmin.ts
@@ -0,0 +1,10 @@
+const DB_CONFIG = {
+  user: "your_db_username",
+  host: "your_db_host",
+  database: "your_db_name",
+  password: "your_db_password",
+  port: 5432,
+};
+
+const ADMIN_USERNAME = "admin1";
+const ADMIN_PASSWORD = "AdminPassword123";
